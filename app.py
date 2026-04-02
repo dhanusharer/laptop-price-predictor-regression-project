5cd3b02f64e4ab26ecea45a20eec79b6df17e142
Dhanush A G <dhanusharer@gmail.com>
2026-04-02 23:16:04 +0530

    adding app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 import streamlit as st
 import pickle
 import numpy as np
+import pandas as pd
 
-# import the model
-pipe = pickle.load(open('pipe.pkl','rb'))
-df = pickle.load(open('df.pkl','rb'))
+st.set_page_config(page_title="Laptop Predictor")
+
+
+def load_artifacts():
+    try:
+        with open("pipe.pkl", "rb") as pipe_file:
+            pipe = pickle.load(pipe_file)
+        with open("df.pkl", "rb") as df_file:
+            df = pickle.load(df_file)
+        return pipe, df
+    except Exception as exc:
+        st.error("Model files could not be loaded. This is usually a scikit-learn version mismatch.")
+        st.code(
+            "pip install -r requirements.txt\n"
+            "python train.py",
+            language="bash",
+        )
+        st.exception(exc)
+        st.stop()
+
+
+pipe, df = load_artifacts()
 
 st.title("Laptop Predictor")
 
@@ -44,8 +64,10 @@ gpu = st.selectbox('GPU',df['Gpu brand'].unique())
 os = st.selectbox('OS',df['os'].unique())
 
 if st.button('Predict Price'):
-    # query
-    ppi = None
+    if weight <= 0:
+        st.error("Please enter a laptop weight greater than 0.")
+        st.stop()
+
     if touchscreen == 'Yes':
         touchscreen = 1
     else:
@@ -59,8 +81,25 @@ if st.button('Predict Price'):
     X_res = int(resolution.split('x')[0])
     Y_res = int(resolution.split('x')[1])
     ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
-    query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
 
-    query = query.reshape(1,12)
-    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
+    query = pd.DataFrame(
+        [[company, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os]],
+        columns=[
+            'Company',
+            'TypeName',
+            'Ram',
+            'Weight',
+            'Touchscreen',
+            'Ips',
+            'ppi',
+            'Cpu brand',
+            'HDD',
+            'SSD',
+            'Gpu brand',
+            'os',
+        ],
+    )
+
+    prediction = int(np.exp(pipe.predict(query)[0]))
+    st.success(f"The predicted price of this configuration is {prediction}")
 
